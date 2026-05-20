20d09f5263d37ef44e176c0d6d47f8c13343d458
Petter-Huun <puhuun@gmail.com>
2026-05-20 13:24:54 +0200

    fikset burgermeny

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const burger = document.getElementById("burger")
+const burgerx = document.getElementById("burgerx")
+const burgermeny = document.getElementById("burgermeny")
 
-function burgermeny() {
-    
-}
 
-console.log(burger)
+burger.addEventListener("click", () => {
+    burgermeny.classList.toggle("visburger")
+    burger.style.display = "none"
+    burgerx.style.display = "block"
+})
+
+burgerx.addEventListener("click", () => {
+    burgermeny.classList.toggle("visburger")
+    burger.style.display = "block"
+    burgerx.style.display = "none"
+})
+
