0807d6c6cb5633990801c3a29a0a8db8cf8c53d5
Petter-Huun <puhuun@gmail.com>
2026-06-05 08:40:14 +0200

    JS til match kortene

--- script.js
+++ script.js
@@ -44,14 +44,43 @@ function visInnhold(trykk) {
 
 // TEST DEG SELV
 
+const kort = document.querySelectorAll(".kort")
+
+function stokkKort () {
+    kort.forEach(element => {
+        let tilfeldig = Math.floor(Math.random() * 18)
+        element.style.order = tilfeldig
+    })
+}
+
+stokkKort()
+
+let kort1 = null
+let kort2 = null 
+
+kort.forEach(elm => {
+    elm.addEventListener("click", () => {
+        console.log("kortet ble klikket")
+        elm.classList.add("trykket")
+
+        if (kort1 === null) {
+            kort1 = elm 
+        } else if (kort2 === null) {
+            kort2 = elm 
+
+            setTimeout(sjekkKort, 500)
+        }
+    })
+})
+
 
 function sjekkKort() {
     let bilde1 = kort1.querySelector(".bak").classList[1] //finner andre klassen i classlisten til .bak
     let bilde2 = kort2.querySelector(".bak").classList[1]
 
     if (bilde1 !== bilde2) {
-        kort1.classList.remove("flip")
-        kort2.classList.remove("flip")
+        kort1.classList.remove("trykket")
+        kort2.classList.remove("trykket")
     } else {
         kort1.classList.add("funnet")
         kort2.classList.add("funnet")
