47c29b1594830ca6c462cea7133a4b7d141b1a07
Petter-Huun <puhuun@gmail.com>
2026-06-05 19:29:06 +0200

    Laget en spørsmålsboks

--- script.js
+++ script.js
@@ -127,12 +127,62 @@ function reset() {
 
 //Spørsmål
 
+let riktig = 0
+let feil = 0
+let spmNr = 0
+const overskrift = document.getElementById("testH2")
+const testTekst = document.querySelector(".pQuiz")
+
+const sporsmal = [
+    {
+        tekst: "Hvilken alpinist vant flest medaljer under OL i Milano/Cortina?",
+        svar: "Franjo Von Allmen"
+    },
+
+    {
+        tekst: "Hvilken alpinist har vunnet flest sammenlagtkuler?",
+        svar: "Marcel Hirscher"
+    },
+
+    {
+        tekst: "Hvilket renn kjøres på kvelden/natten",
+        svar: "Schladming"
+    },
+
+    {
+        tekst: "Hvilken alpinist fikk flest WC points i forrige sesong?",
+        svar: "Marco Odermatt"
+    },
+
+    {
+        tekst: "Hvilken alpinist har flest WC seiere?",
+        svar: "Michaela Shiffrin"
+    }
+]
 
 function sjekk(elm) {
     const input = elm.previousElementSibling
-    const svar = input.value
+    const svar = input.value    
     console.log(svar)
-    const output = document.querySelector("div > h1")
-    output.innerHTML = "Du skrev: " + svar
+    if (svar === sporsmal[spmNr].svar) {
+        riktig += 1
+    } else {
+        feil += 1
+    }
+
+    input.value = ""
+
+    spmNr += 1
+
+    if (spmNr < sporsmal.length) {
+        overskrift.innerHTML = "Spørsmål " + (spmNr + 1) + "/5"
+        testTekst.innerHTML = sporsmal[spmNr].tekst
+        input.value = ""
+    } else {
+        overskrift.innerHTML = "Quizen er ferdig!"
+        testTekst.innerHTML = "Du fikk " + riktig + "av 5"
+        input.value = ""
+    }
+    
 }
 
